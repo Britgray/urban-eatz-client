5acd9d7a44d99c515ff8454ef432288d9fd3b1c1
Britgray <britgray2000@gmail.com>
2024-01-18 21:38:18 -0500

    updated folders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+  import "./App.css";
+  import Home from "./Home";
+  import ClientsView from "./component/client/ClientsView";
+  import NavBar from "./Common/NavBar";
+  import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+  } from "react-router-dom";
+  import AddClient from "./component/client/AddClient";
+  import EditClient from "./component/client/EditClient.js";
+  import ClientProfile from "./component/client/ClientProfile.js";
+  
+  function App() {
+    return (
+      <main className="container mt-5">
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={<Home />}></Route>
+            <Route
+              exact
+              path="/view-clients"
+              element={<ClientsView />}></Route>
+            <Route
+              exact
+              path="/add-clients"
+              element={<AddClient />}></Route>
+            <Route
+              exact
+              path="/edit-client/:id"
+              element={<EditClient />}></Route>
+            <Route
+              exact
+              path="/client-profile/:id"
+              element={<ClientProfile />}></Route>
+          </Routes>
+        </Router>
+      </main>
+    );
+  }
+  
+  export default App;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
---
+++ src/component/client/AddClient.js
@@ -0,0 +1,150 @@
+import { useState } from "react";
+import {
+	Link,
+	useNavigate,
+} from "react-router-dom";
+import axios from "axios";
+
+const AddClient = () => {
+	let navigate = useNavigate();
+	const [client, setClient] = useState({
+		firstName: "",
+		lastName: "",
+        nameOfBusiness:"",
+		email: "",
+		category: "",
+	});
+	const {
+		firstName,
+		lastName,
+        nameOfBusiness,
+		email,
+		category,
+	} = client;
+
+	const handleInputChange = (e) => {
+		setClient({
+			...client,
+			[e.target.name]: e.target.value,
+		});
+	};
+	const saveClient = async (e) => {
+		e.preventDefault();
+		await axios.post(
+			"http://localhost:3000/clients",
+			client
+		);
+		navigate("/view-clients");
+	};
+
+	return (
+		<div className="col-sm-8 py-2 px-5 offset-2 shadow">
+			<h2 className="mt-5"> Add Client</h2>
+			<form onSubmit={(e) => saveClient(e)}>
+				<div className="input-group mb-5">
+					<label
+						className="input-group-text"
+						htmlFor="fristName">
+						First Name
+					</label>
+					<input
+						className="form-control col-sm-6"
+						type="text"
+						name="firstName"
+						id="firstName"
+						required
+						value={firstName}
+						onChange={(e) => handleInputChange(e)}
+					/>
+				</div>
+
+				<div className="input-group mb-5">
+					<label
+						className="input-group-text"
+						htmlFor="lastName">
+						Last Name
+					</label>
+					<input
+						className="form-control col-sm-6"
+						type="text"
+						name="lastName"
+						id="lastName"
+						required
+						value={lastName}
+						onChange={(e) => handleInputChange(e)}
+					/>
+				</div>
+                <div className="input-group mb-5">
+					<label
+						className="input-group-text"
+						htmlFor="nameOfBusiness">
+						Bussiness name
+					</label>
+					<input
+						className="form-control col-sm-6"
+						type="text"
+						name="nameOfBusiness"
+						id="nameOfBusiness"
+						required
+						value={nameOfBusiness}
+						onChange={(e) => handleInputChange(e)}
+					/>
+				</div>
+				<div className="input-group mb-5">
+					<label
+						className="input-group-text"
+						htmlFor="email">
+						Your Email
+					</label>
+					<input
+						className="form-control col-sm-6"
+						type="email"
+						name="email"
+						id="email"
+						required
+						value={email}
+						onChange={(e) => handleInputChange(e)}
+					/>
+				</div>
+
+				<div className="input-group mb-5">
+					<label
+						className="input-group-text"
+						htmlFor="category">
+						Category
+					</label>
+					<input
+						className="form-control col-sm-6"
+						type="text"
+						name="category"
+						id="category"
+						required
+						value={category}
+						onChange={(e) => handleInputChange(e)}
+					/>
+				</div>
+
+				<div className="row mb-5">
+					<div className="col-sm-2">
+						<button
+							type="submit"
+							className="btn btn-outline-success btn-lg">
+							Save
+						</button>
+					</div>
+
+					<div className="col-sm-2">
+						<Link
+							to={"/view-clients"}
+							type="submit"
+							className="btn btn-outline-warning btn-lg">
+							Cancel
+						</Link>
+					</div>
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default AddClient;
